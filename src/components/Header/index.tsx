11a55b74a8dedb44769b9df835fef3e7119db84c
Rong9394 <148251361+Rong9394@users.noreply.github.com>
2023-10-25 11:33:36 -0400

    added select due date button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,9 @@ import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
 import { useState } from "react";
+import { format } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 
 export function Header({updateList}:{updateList:(title:string)=>void}) {
@@ -9,6 +12,8 @@ export function Header({updateList}:{updateList:(title:string)=>void}) {
   const [buttonDiabled, setState] = useState({isEmpty: true, cursorStyle: 'not-allowed'});
   
   const [title, setTitle] = useState('');
+
+  const [dueDate, setDueDate] = useState<Date>();
 
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     setTitle((e.target as HTMLInputElement).value);
@@ -25,7 +30,10 @@ export function Header({updateList}:{updateList:(title:string)=>void}) {
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm}>
-        <input onChange={(e) => onChange(e)} placeholder="Add a new assignment" type="text" />
+        <div>
+          <input onChange={(e) => onChange(e)} placeholder="Add a new assignment" type="text" />
+          <button id={styles.datePicker}>Select due date</button>
+        </div>
         <button type={"button"} onClick={()=>updateList(title)} disabled={buttonDiabled['isEmpty']} style={{cursor:buttonDiabled['cursorStyle']}}>
           Create <AiOutlinePlusCircle size={20} />
         </button>
